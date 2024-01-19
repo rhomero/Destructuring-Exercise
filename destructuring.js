ddd86d36230d50a531bb2ff568391b41f4b9c0e6
Homero Ramirez <homeroram@outlook.com>
2024-01-18 20:03:54 -0600

    es2015 edits

--- destructuring.js
+++ destructuring.js
@@ -52,3 +52,26 @@ console.log(third); // ? "Chi"
 [numbers[1], numbers[2]] = [numbers[2], numbers[1]]
 
 console.log(numbers) // ? [10,30,20]
+
+//es5 vers
+var obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+  var a = obj.numbers.a;
+  var b = obj.numbers.b;
+
+  /* Write an ES2015 Version */
+  const numb = {
+    numbers: {
+        a: 1,
+        b: 2
+    }
+  };
+
+  const {a,b} = obj.number;
+  //console.log({numbers:{a}, numbers:{b}} = numb);
+  
